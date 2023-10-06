0b0748a5551709b52a7917422810ec3e666922af
Nidhi Solanki <91738026+Nidhi2215@users.noreply.github.com>
2023-10-07 01:47:51 +0530

    Create removeDigit.cpp

---
+++ removeDigit.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string removeDigit(string number, char digit) 
+    {
+        int count=0,iter=1;;
+        int s[number.size()];
+        for (int i = 0; i < number.length(); ++i) 
+        {
+            if (number[i] == digit) 
+            {
+                count++;
+            }
+        }
+        for (int i = 0; i < number.length(); ++i) 
+        {
+            if (number[i] == digit && iter==count) 
+            {
+                number.erase(number.begin() + i);
+                break;
+            }
+            else if(number[i] == digit && iter!=count)
+            {
+                if(number[i+1]>number[i]) 
+                    {
+                        number.erase(number.begin() + i);
+                        break;
+                    }
+                else
+                {
+                    iter++;
+                    continue;
+                }
+            }
+            
+        }
+    return number;
+    }
+};
